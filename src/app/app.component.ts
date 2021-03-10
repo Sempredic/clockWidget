2dd5038ed4f50d81219158d209f9a032bc2140ea
Vincent Espinoza <vincent.espinoza@yahoo.com>
2021-03-10 11:57:10 -0600

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+
+  date:string;
+  hours:any;
+  minutes:any;
+  seconds:any;
+  currentLocale: any;
+
+  isTwelveHrFormat:false;
+  test:any;
+  constructor(){
+   //   let now = moment(); // add this 2 of 4
+     //navigator.language || navigator.userLanguage;
+
+ //var test = moment('2016-01-24 14:23:45');
+
+       //ja-JP;
+ //de-DE
+    setInterval(() =>{
+   const currentDate = new Date();
+   this.date = currentDate.toLocaleTimeString();
+    }, 1000);
+  }
 }
