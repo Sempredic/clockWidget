caf6c24b611174b6e0fbeea15442131e6728a3c2
Vincent Espinoza <vincent.espinoza@yahoo.com>
2021-03-11 06:35:44 -0600

    Fifth

--- server.js
+++ server.js
@@ -5,16 +5,48 @@ function requireHTTPS(req, res, next) {
     }
     next();
 }
+
+const dotenv = require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {Pool} = require('pg');
+
 const app = express();
+
 //app.use(requireHTTPS);
 
 app.use(express.static('./dist/my-first-project'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
-app.get('/*', function(req, res) {
+
+app.get('/', function(req, res) {
     res.sendFile('index.html', {root: 'dist/my-first-project/'});
 });
 
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
+ ssl: {
+  rejectUnauthorized: false
+ }
+});
 
 
-app.listen(process.env.PORT || 8080);
+app.get('/api/convos', function (req, res) {
+  console.log('IN');
+  pool.query('SELECT Name FROM salesforce.ConvoObj__c;', (error, results) => {
+    if (error) {
+      console.log("Error - Failed to select all from Users");
+      console.log(error);
+    }else{
+        console.log(results.rows);
+    }
+    res.status(200).json(results.rows)
+  });
+})
+
+
+app.listen(process.env.PORT);
